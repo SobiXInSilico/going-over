8c524069811b1121632fc6ec656f45c848133f8f
SobiX@Home <siciliansilicon@gmail.com>
2023-03-10 14:28:01 +0330

    doc fixed-desc-add

--- go-prologs/keyboard/keyboard.go
+++ go-prologs/keyboard/keyboard.go
@@ -1,5 +1,5 @@
-//Package comments should begin with “Package” followed by the package name: 
 //Package keyboard reads user input from the keyboard.
+//Package comments should begin with “Package” followed by the package name.
 
 package keyboard
 
@@ -10,9 +10,9 @@ import (
 	"strings"
 )
 
-//Function comments should begin with the name of the function they describe:
-//GetFloat reads a floating-point number from the keyboard.
-//It returns the number read and any error encountered. 
+// Function comments should begin with the name of the function they describe:
+// GetFloat reads a floating-point number from the keyboard.
+// It returns the number read and any error encountered.
 func GetFloat() (float64, error) {
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
