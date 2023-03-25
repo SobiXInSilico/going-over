0babe5d9a998965012d69611af6daa70b74cd82e
SobiX@Home <siciliansilicon@gmail.com>
2023-03-25 20:42:32 +0330

    go-prologs

---
+++ go-prologs/Head-First-Go/vote-count/main.go
@@ -0,0 +1,17 @@
+// count tallies the number of times each line
+// occurs within a file
+package main
+
+import (
+	"fmt"
+	"github.com/SicilianSilicon/going-over/tree/main/go-prologs/Head-First-Go/datafile"
+	"log"
+)
+
+func main() {
+	lines, err := datafile.GetStrings("votes.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(lines)
+}
